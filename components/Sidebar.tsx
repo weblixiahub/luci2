80ff3b970ca143eca7a5b9cf83ed2f1b9ac76ed0
weblixiahub <alecsignupacc@gmail.com>
2026-03-03 03:28:37 -0500

    Reapply "Fix: restore sidebar footer while keeping nav clean"
    This reverts commit 7c237058f7408a76d4cc0676c3d87a4c5d70044c.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { SiDiscord, SiFacebook, SiInstagram, SiTiktok, SiX } from 'react-icons/si';
 
 type SidebarProps = {
   open: boolean;
@@ -9,7 +10,7 @@ type SidebarProps = {
 
 const navItems = [
   ['Text Me Hotties <3', 'https://laylo.com/luci'],
-  ['Email Me', '/links'],
+  ['Email Me', 'mailto:luciofficialmusic13@gmail.com'],
   ['Book Me', '/book-me'],
   ['Links', '/links']
 ];
@@ -34,6 +35,25 @@ export default function Sidebar({ open, close }: SidebarProps) {
           </Link>
         ))}
       </nav>
+      <div className="sidebarFooter">
+        <div className="sidebarSocials">
+          <a href="https://www.instagram.com/luci_official/" target="_blank" rel="noreferrer" aria-label="Instagram">
+            <SiInstagram />
+          </a>
+          <a href="https://www.tiktok.com/@luci.official" target="_blank" rel="noreferrer" aria-label="TikTok">
+            <SiTiktok />
+          </a>
+          <a href="https://x.com/Luci__official" target="_blank" rel="noreferrer" aria-label="X">
+            <SiX />
+          </a>
+          <a href="https://www.facebook.com/Luciofficial" target="_blank" rel="noreferrer" aria-label="Facebook">
+            <SiFacebook />
+          </a>
+          <a href="https://discord.com/invite/hXBavTEghd" target="_blank" rel="noreferrer" aria-label="Discord">
+            <SiDiscord />
+          </a>
+        </div>
+      </div>
     </aside>
   );
 }
